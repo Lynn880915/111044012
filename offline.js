5074b277bfc1bd505211e95d83245d306c016c3a
Lynn880915 <102577667+Lynn880915@users.noreply.github.com>
2022-03-29 15:14:11 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1648537420,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/hammer-sheet0.png",
+		"images/mouse-sheet0.png",
+		"images/hole-sheet0.png",
+		"media/laserretro_001.ogg",
+		"media/laserretro_001.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
